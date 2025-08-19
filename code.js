1adea073942371476715b10da0315c9e491a8558
thunder-ru <rosanov.danila2016@yandex.ru>
2025-08-19 14:43:32 +0300

    code.js

--- code.js
+++ code.js
@@ -118,7 +118,7 @@ if (track && container) {
 const projectData = {
   car_rental: {
     title: "Аренда автомобилей",
-    images: ["https://i.postimg.cc/pdkWMT84/Black1.jpg"], "https://i.postimg.cc/tR8BKPyZ/Blakc2.jpg", "https://i.postimg.cc/PrmQ0R29/Black3.jpg", "https://i.postimg.cc/0yndyJgC/Black4.jpg", "https://i.postimg.cc/HnbbDr4X/Black-5.jpg"
+    images: ["https://i.postimg.cc/pdkWMT84/Black1.jpg"], "https://i.postimg.cc/tR8BKPyZ/Blakc2.jpg", "https://i.postimg.cc/PrmQ0R29/Black3.jpg", "https://i.postimg.cc/0yndyJgC/Black4.jpg", "https://i.postimg.cc/HnbbDr4X/Black-5.jpg"]
   },
   dentistry: {
     title: "Стоматология",
